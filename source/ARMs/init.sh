410fd845082a8b582b833874378c68391605fdb5
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2017-12-27 13:35:18 +0300

    fix init.sh

--- source/ARMs/init.sh
+++ source/ARMs/init.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-## params
-usr = $1
-password = $2
+## Get params
+usr=$1
+password=$2
+rstudio="rstudio-server-"$3"-amd64.deb"
 
 
-## install last updates
+## Install last updates
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 
 
 ## configure One-Box ML Server
-/usr/local/bin/dotnet /opt/microsoft/mlserver/9.2.1/o16n/Microsoft.MLServer.Utils.AdminUtil/Microsoft.MLServer.Utils.AdminUtil.dll -silentoneboxinstall $password
+/usr/local/bin/dotnet /opt/microsoft/mlserver/9.2.1/o16n/Microsoft.MLServer.Utils.AdminUtil/Microsoft.MLServer.Utils.AdminUtil.dll -silentoneboxinstall "$password"
 iptables --flush
 
 
-## install R dependencies
-# RStudio installer
-apt-get -y install gdebi-core
+## Install R dependencies
 # R-packages (common)
 apt-get -y install build-essential
 apt-get -y install libcurl4-openssl-dev libssl-dev
 apt-get -y install gfortran
+
 # LightGBM
-apt-get -y install git
 mkdir -p /home/$usr/apps/LightGBM
 git clone --recursive https://github.com/Microsoft/LightGBM /home/$usr/apps/LightGBM
 apt-get -y install cmake
 apt-get -y install glibc-source
 
-## dev tools
+# RStudio Server installer
+apt-get -y install gdebi-core
+wget https://download2.rstudio.org/$rstudio
+gdebi --n $rstudio
+rstudio-server verify-installation
+rm $rstudio
+
+# tools
 apt-get -y install htop
