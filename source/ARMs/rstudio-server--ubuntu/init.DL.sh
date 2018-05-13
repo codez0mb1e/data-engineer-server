e8d8f1c9de0eab3604909b052f426e889949ff78
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2018-05-13 19:18:22 +0300

    Remove not DL framework installation commands

--- source/ARMs/rstudio-server--ubuntu/init.DL.sh
+++ source/ARMs/rstudio-server--ubuntu/init.DL.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-### summary: install DL-frameworks and R tools
-
+### summary: install DL-frameworks
 
 ## install updates ----
 apt-get -y update
 apt-get -y dist-upgrade
-
-## core libs ----
-apt-get -y install build-essential libssl-dev cmake
-
 
 
 ## install Tensorflow ----
@@ -23,7 +18,6 @@ easy_install -U pip
 
 # install TensorFlow in the active Virtualenv environment [3]
 pip3 install --upgrade tensorflow-gpu
-
 
 
 ## install Keras ----
@@ -44,54 +38,6 @@ pip install keras #? pip3
 pip install keras --upgrade pip #? pip3 
 
 
-
-## install R and R tools ----
-wget  https://mran.blob.core.windows.net/install/mro/3.4.4/microsoft-r-open-3.4.4.tar.gz
-tar -xf microsoft-r-open-3.4.4.tar.gz
-cd microsoft-r-open/
-./install.sh -a -u
-
-R --version
-
-
-# RStudio Server
-apt-get install gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.1.447-amd64.deb
-gdebi rstudio-server-1.1.447-amd64.deb
-
-rstudio-server status
-
-
-# R-packages dependencies
-apt-get -y install gfortran libcurl4-openssl-dev 
-
-# install SQL Server drivers
-sudo su 
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-exit 
-
-apt-get update
-ACCEPT_EULA=Y apt-get install msodbcsql
-
-apt-get -y install unixodbc unixodbc-dev #? tdsodbc
-
-
-
-## install LightGBM ----
-# see for GPU version install: http://lightgbm.readthedocs.io/en/latest/Installation-Guide.html#id6
-
-# CPU version install
-git clone --recursive https://github.com/Microsoft/LightGBM ; cd LightGBM
-mkdir build ; cd build
-cmake ..
-make -j4
-
-# or run in RStudio
-#    library(devtools)
-#    options(devtools.install.args = "--no-multiarch") # if you have 64-bit R only, you can skip this
-#    install_github("Microsoft/LightGBM", subdir = "R-package")
-
 ## install CNTK (todo: section needs to be checked) ----
 apt-get -y install libopencv-dev python-opencv
 
@@ -108,26 +54,6 @@ pip install https://cntk.ai/PythonWheel/CPU-Only/cntk-2.3.1-cp36-cp36m-linux_x86
 R
 > Sys.setenv(RETICULATE_PYTHON="/home/dp/anaconda3/envs/cntk-py36/bin/python")
 > reticulate::py_module_available("cntk")
-
-
-
-## install tools ----
-apt-get -y install htop # and git, nvidia-smi if not already installed
-
-adduser <user>
-
-git --version
-git config --global user.name "Dmitry Petukhov"
-git config --global user.email "dpetukhov@***.com"
-git config --list
-
-# see more on https://docs.microsoft.com/en-us/vsts/git/use-ssh-keys-to-authenticate?view=vsts
-ssh-keygen -C "dpetukhov@***.com"
-cat /home/<user>/.ssh/id_rsa.pub
-
-cd ./apps
-git clone ssh://<server>@vs-ssh.visualstudio.com:22/DefaultCollection/_ssh/<repo>
-
 
 
 ## References: ----
