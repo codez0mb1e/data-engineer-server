8d7830baf4b0f369721561a72da63bd88894a5b6
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2019-01-22 19:25:40 +0300

    Minor updates

--- deployment/install_scripts/install_r.sh
+++ deployment/install_scripts/install_r.sh
@@ -7,7 +7,7 @@
 
 
 # R-packages dependencies ----
-apt-get -y install  gfortran libcurl4-openssl-dev libxml2-dev
+apt-get -y install gfortran libcurl4-openssl-dev libxml2-dev
 
 
 
@@ -16,15 +16,16 @@ wget https://mran.blob.core.windows.net/install/mro/3.5.1/microsoft-r-open-3.5.1
 tar -xf microsoft-r-open-3.5.1.tar.gz
 cd microsoft-r-open
 ./install.sh -a -u
-cd ~
 
 R --version
+cd ~
 
 
-# Install RStudio Server
+
+# Install RStudio Server ----
 apt-get -y install gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.1.456-amd64.deb
-gdebi -q rstudio-server-1.1.456-amd64.deb
+wget https://download2.rstudio.org/rstudio-server-1.1.463-amd64.deb
+gdebi --quiet rstudio-server-1.1.463-amd64.deb
 
 rstudio-server status
 
@@ -33,7 +34,7 @@ rstudio-server status
 # Install SQL Server drivers
 sudo su 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 exit 
 apt-get update
 ACCEPT_EULA=Y apt-get install msodbcsql
