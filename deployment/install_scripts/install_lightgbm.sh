c4f9eabb229757b9faef94f7ecd01b4b4dcc5de6
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2019-01-24 20:44:25 +0300

    Update lightgbm installation script

--- deployment/install_scripts/install_lightgbm.sh
+++ deployment/install_scripts/install_lightgbm.sh
@@ -5,21 +5,25 @@
 # 
 
 
-# install Boost and OpenCL
-apt-get -y install libboost-all-dev ocl-icd-opencl-dev 
+# Install dependecies ----
+apt-get -y install ocl-icd-opencl-dev libboost-dev libboost-system-dev libboost-filesystem-dev
+Rscript -e "install.packages(c('roxygen2', 'data.table', 'devtools'))"
 
+
+# Install LightGBM ----
+# WARN: go to RStudio terminal
 # clone LightGBM
 git clone --recursive https://github.com/Microsoft/LightGBM
+# set use_gpu to TRUE in R-package/src/install.libs.R 
 cd LightGBM
+Rscript build_r.R
+cd ~
 
-# build LightGBM
-mkdir build ; cd build
-cmake -DUSE_GPU=1 ..
-make -j4
 
-# test
-# Rscript "install.packages('lightgbm', destdir = 'home/<user>/LightGBM/build'); library(lightgbm)"
+# Validate installation ----
+Rscript -e "library(lightgbm)"
 
 
 # References ----
-# 1. https://github.com/Microsoft/LightGBM/blob/master/docs/Installation-Guide.rst#build-gpu-version
+# 1. https://lightgbm.readthedocs.io/en/latest/Installation-Guide.html#id17
+# 2. https://github.com/Microsoft/LightGBM/blob/master/docs/Installation-Guide.rst#build-gpu-version
