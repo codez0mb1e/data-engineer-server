5d9849d965dd7d1cf84b1e128b9367dc66ef0a4b
Dmitry Petukhov <dmitry.petukhov@live.ru>
2019-03-20 13:56:27 +0300

    Update Tensorflow install script

--- scripts/install_tensorflow.sh
+++ scripts/install_tensorflow.sh
@@ -10,41 +10,30 @@ python3 --version
 pip3 --version
 virtualenv --version
 
-apt -y install python3-dev python3-pip
+apt install -y python3-dev python3-pip
 pip3 install -U virtualenv
 
 
 
 # Install Tensorflow ---- 
-# tensorflow-gpu cannot work with CUDA 10 [https://github.com/tensorflow/tensorflow/issues/22940]
-pip3 install tf-nightly-gpu # tensorflow-gpu 
+pip3 install tensorflow-gpu
 
 # validate installation 
-python3 -c "import tensorflow as tf; print(tf.__version__)"
-
+python3 -c "import tensorflow as tf; tf.enable_eager_execution(); print(tf.reduce_sum(tf.random_normal([1000, 1000])))"
 
 
 # Install Keras ----
-# NOTE: before installing Keras, please install one of its backend engines: TensorFlow, Theano, or CNTK
-
-# install a BLAS library to ensure that you can run fast tensor operations on your CPU [3]
-apt -y install unzip pkg-config libopenblas-dev liblapack-dev
-
-# install the Python scientific suite [3]
-apt -y install python-numpy python-scipy python-matplotlib python-yaml
 
 # install HDF5. It will allow you to save your Keras models to disk quickly and efficiently [3]
-apt -y install libhdf5-serial-dev python-h5py
+apt install -y libhdf5-serial-dev python-h5py
 
 # install Keras
 pip3 install keras
-
-# validate installation
-Rscript -e "library(keras); install_keras(tensorflow = 'gpu')"
 
 
 
 # References ----
 # 1. https://www.tensorflow.org/install/install_linux#NVIDIARequirements
-# 2. https://keras.io/#installation
-# 3. https://livebook.manning.com/#!/book/deep-learning-with-r/appendix-a/1
+# 2. https://www.tensorflow.org/install/pip
+# 3. https://keras.io/#installation
+# 4. https://livebook.manning.com/#!/book/deep-learning-with-r/appendix-a/1
