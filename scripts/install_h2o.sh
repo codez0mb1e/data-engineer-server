5a6b50c6f647e23a9e5521642b278cc5bbe380ed
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2019-10-04 17:16:50 +0300

    Update H2O version

--- scripts/install_h2o.sh
+++ scripts/install_h2o.sh
@@ -5,9 +5,9 @@
 #
 
 
-H2O_VERSION="3.26.0.5"; readonly H2O_VERSION
+H2O_VERSION="3.26.0.6"; readonly H2O_VERSION
 
-wget http://h2o-release.s3.amazonaws.com/h2o/rel-yau/5/h2o-${H2O_VERSION}.zip
+wget http://h2o-release.s3.amazonaws.com/h2o/rel-yau/6/h2o-${H2O_VERSION}.zip
 unzip h2o-${H2O_VERSION}.zip
 
 cd h2o-${H2O_VERSION}
@@ -16,4 +16,4 @@ java -jar h2o.jar
 
 
 # References ----
-# 1. http://h2o-release.s3.amazonaws.com/h2o/rel-yau/5/index.html
+# 1. http://h2o-release.s3.amazonaws.com/h2o/rel-yau/6/index.html
