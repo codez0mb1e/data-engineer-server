f53fd59039d97e361bbb5d3372617985b51af33e
codez0mb1e <dmitry.petukhov@outlook.com>
2019-11-21 14:41:07 +0000

    Add install python tools script

---
+++ scripts/install_python_tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Install python tools
+#
+
+apt-get install python3-tk 
+
+
+# Set correct reticalte version in RStudio
+#> library(reticulate)
+#> use_python(python = Sys.which("python3"), required = TRUE)
+#> py_config()
