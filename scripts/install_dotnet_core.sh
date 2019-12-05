52dc358cb475e4c5860c773744616aced3d03f30
codez0mb1e <dmitry.petukhov@outlook.com>
2019-12-05 09:11:31 +0000

    Update to .net core 3.1

--- scripts/install_dotnet_core.sh
+++ scripts/install_dotnet_core.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 #
-# Install .NET Core 2.2
+# Install .NET Core 3.1
 #
 
 
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 
+add-apt-repository universe
 apt update
-apt install -y dotnet-runtime-2.2
+
+apt install apt-transport-https
+apt update
+
+apt install dotnet-runtime-3.1
 
 
 # References ----
-# 1. https://dotnet.microsoft.com/download/linux-package-manager/ubuntu18-04/runtime-current
+# 1. https://docs.microsoft.com/ru-ru/dotnet/core/install/linux-package-manager-ubuntu-1804
