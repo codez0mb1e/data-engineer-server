ef06e640a3fbd61071b4c1c4d9d0056a52b2efdb
codez0mb1e <dmitry.petukhov@outlook.com>
2020-01-09 15:50:54 +0000

    Minor changes

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,3 +17,13 @@ apt install -y build-essential libssl-dev cmake
 
 # Install utils ----
 apt install -y htop ncdu
+
+
+# Other  ----
+# mount data disc (optional)
+sudo mount /dev/sdc1 /datadrive
+df - Th
+
+# add users (optional)
+adduser <user_name>
+usermod -aG sudo <user_name>
