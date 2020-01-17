759dc548cea686fcdc1cd3813f9cec6555298146
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2020-01-17 15:18:06 +0300

    Update to last version

--- scripts/install_h2o.sh
+++ scripts/install_h2o.sh
@@ -5,15 +5,19 @@
 #
 
 
-H2O_VERSION="3.26.0.6"; readonly H2O_VERSION
+# 1. Download and unpack
+H2O_VERSION="3.28.0.1"; readonly H2O_VERSION
 
-wget http://h2o-release.s3.amazonaws.com/h2o/rel-yau/6/h2o-${H2O_VERSION}.zip
+wget http://h2o-release.s3.amazonaws.com/h2o/rel-yu/1/h2o-${H2O_VERSION}.zip
 unzip h2o-${H2O_VERSION}.zip
 
 cd h2o-${H2O_VERSION}
 
+# 2. Install
 java -jar h2o.jar
+
+# 3. Point your browser to http://localhost:54321
 
 
 # References ----
-# 1. http://h2o-release.s3.amazonaws.com/h2o/rel-yau/6/index.html
+# 1. http://h2o-release.s3.amazonaws.com/h2o/latest_stable.html
