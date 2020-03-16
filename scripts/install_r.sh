36e9052b54655e22511e14928c27e01ca3b29985
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2020-03-16 12:36:22 +0300

    Minor fixes

--- scripts/install_r.sh
+++ scripts/install_r.sh
@@ -6,7 +6,7 @@
 
 
 # Set params ----
-USR="dp"; readonly USR
+USR="<user_name>"; readonly USR
 R_VERSION="3.5.3"; readonly R_VERSION
 RSTUDIO_SERVER_VERSION="1.2.5033"; readonly RSTUDIO_SERVER_VERSION
 
@@ -27,8 +27,6 @@ wget https://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1
 dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
 
 # validate R Open installation
-rstudio-server verify-installation
-# or
 R --version 
 
 
