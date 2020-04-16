9cb38a6e446338768bbba5597d4e0b954fb4fe5a
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2020-04-16 12:26:36 +0300

    Update h2o version

--- scripts/install_h2o.R
+++ scripts/install_h2o.R
@@ -2,6 +2,11 @@
 #' Install or update H2O package
 #'
 
+
+# H2O repo url
+repo_url <- "http://h2o-release.s3.amazonaws.com/h2o/rel-zahradnik/1/R" #! set valid url - check it on [1]
+
+
 # The following two commands remove any previously installed H2O packages for R.
 if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
 if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
@@ -9,16 +14,17 @@ if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
 # Next, we download packages that H2O depends on.
 pkgs <- c("RCurl","jsonlite")
 for (pkg in pkgs) {
-  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
 }
 
 # Now we download, install and initialize the H2O package for R.
-install.packages("h2o", type="source", repos="http://h2o-release.s3.amazonaws.com/h2o/rel-yu/2/R")
+install.packages("h2o", type="source", repos=repo_url)
 
 # Finally, let's load H2O and start up an H2O cluster
 library(h2o)
 h2o.init()
 
 
+
 # References
-# 1. http://h2o-release.s3.amazonaws.com/h2o/rel-yu/2/index.html
+# 1. http://h2o-release.s3.amazonaws.com/h2o/latest_stable.html
