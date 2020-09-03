ec1a87a66bbe4bd37f9175edfa52cd3aba21020c
codez0mb1e <dmitry.petukhov@outlook.com>
2020-09-03 16:01:57 +0000

    Update .NET install script + add Powershell Core

--- scripts/install_dotnet_core.sh
+++ scripts/install_dotnet_core.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
 #
-# Install .NET Core 3.1
+# Install .NET Core 3.1 and Powershell Core
 #
 
 
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# .NET Core ----
+
+# 1. Download 
+
+# Download Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
 dpkg -i packages-microsoft-prod.deb
 
 
-add-apt-repository universe
+# 2. Install
 apt update
+apt install -y apt-transport-https
 
-apt install apt-transport-https
 apt update
+apt install -y dotnet-sdk-3.1
 
-apt install dotnet-runtime-3.1
 
-
-# Validate
+# 3. Validate
 dotnet new console
 dotnet run
 
 
+# PowerShell Core ----
+
+# 1. Enable the "universe" repositories
+add-apt-repository universe
+
+# 2. Install
+apt install -y powershell
+
+# 3. Validate
+pwsh
+
+
 # References ----
-# 1. https://docs.microsoft.com/ru-ru/dotnet/core/install/linux-package-manager-ubuntu-1804
+# 1. https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#1804-
+# 2. https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7#installation-via-package-repository---ubuntu-1804
