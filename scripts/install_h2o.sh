390fba622e8b897333e94213478be314e642022a
codez0mb1e <dmitry.petukhov@outlook.com>
2020-09-03 16:00:22 +0000

    Update H2O install script

--- scripts/install_h2o.sh
+++ scripts/install_h2o.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
 #
-# Install H2O
+# Install H2O framework
 #
 
 
 # 1. Download and unpack
-H2O_VERSION="3.30.0.1"; readonly H2O_VERSION
+H2O_VERSION="3.30.1.1"; readonly H2O_VERSION  # note: check number of latest version [1]
+
 
 wget http://h2o-release.s3.amazonaws.com/h2o/rel-zahradnik/1/h2o-${H2O_VERSION}.zip
 unzip h2o-${H2O_VERSION}.zip
 
 cd h2o-${H2O_VERSION}
 
+
 # 2. Install
 java -jar h2o.jar
 
-# 3. Point your browser to http://localhost:54321
+
+# 3. Create SSH tunnel and point your browser to http://localhost:54321
+
 
 # 4. Install H2O R-package
-Rscript script/install_r_packages.R
+cd R
+
+Rscript -e "install.packages(c('RCurl', 'jsonlite'))"
+R CMD INSTALL h2o_${H2O_VERSION}.tar.gz
+
+
+# 5. Test installation
+Rscript -e "library(h2o); h2o.init()"
 
 
 # References ----
