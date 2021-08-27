1d58d7fd3cbcce2e4607d2f5cd9a92343466ea47
codez0mb1e <dmitry.petukhov@outlook.com>
2021-08-27 19:13:17 +0300

    Minor changes

--- scripts/git_configure.sh
+++ scripts/git_configure.sh
@@ -6,29 +6,30 @@
 
 
 # Set params ----
-USER_NAME="<user_name>"; readonly USER_NAME
-USER_EMAIL="<user@email>"; readonly USER_EMAIL
+USER_NAME="<name>"; readonly USER_NAME
+USER_EMAIL="<email>"; readonly USER_EMAIL
 
 
-# Config git
+# Config git ----
 # WARN: execute commands under created RStudio Server user
 git config --global user.name $USER_NAME
 git config --global user.email $USER_EMAIL
-git config --list
-
 git config --global credential.helper 'cache --timeout=10000000'
 
+git config --list
 
-# Set SSH key to github
+
+# Set SSH key to github ----
+ls -l ~/.ssh/id_*.pub
 ssh-keygen -C $USER_NAME
 cat ~/.ssh/id_rsa.pub # copy output and add to SSH keys [2]
 
 
-# Prepare to clone repos
+# Prepare to clone repos ----
 mkdir ~/repos ; cd ~/repos
 
-# Now you can clone repo:
-# > git clone $repo_url
+# Now you can clone repo (for example this repo):
+git clone git@github.com:codez0mb1e/cloud-rstudio-server.git
 
 
 # References ----
