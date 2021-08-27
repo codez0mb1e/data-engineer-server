9d42503093a2bed77be70c2d6be905e19b57bd28
codez0mb1e <dmitry.petukhov@outlook.com>
2021-08-27 08:54:22 +0300

    Update to Ubuntu 20.04

--- scripts/install_cuda_drivers.sh
+++ scripts/install_cuda_drivers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #
-# Install CUDA drivers v10.0
+# Install CUDA drivers
 #
 
 
 # Set params ----
-OS="ubuntu1804"; readonly OS
+OS="ubuntu2004"; readonly OS
+CUDNN_VERSION="8.2.2.*"
+CUDA_VERSION="cuda11.4"
 
 
 # Check hardware ----
@@ -14,47 +16,31 @@ lspci | grep -i NVIDIA
 
 
 # Download and install the CUDA drivers ----
+wget https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 
-# Add NVIDIA package repositories
-wget https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-repo-${OS}_10.0.130-1_amd64.deb
+sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/ /"
 
-dpkg -i cuda-repo-${OS}_10.0.130-1_amd64.deb
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda nvidia-cuda-toolkit
 
-apt update
 
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/${OS}/x86_64/nvidia-machine-learning-repo-${OS}_1.0.0-1_amd64.deb
+# Install cuDNN ----
+sudo apt install libcudnn8=${CUDNN_VERSION}-1+${CUDA_VERSION}
+sudo apt install libcudnn8-dev=${CUDNN_VERSION}-1+${CUDA_VERSION}
 
-apt install -y ./nvidia-machine-learning-repo-${OS}_1.0.0-1_amd64.deb
-apt update
-
-# Install NVIDIA driver
-apt install -y --no-install-recommends nvidia-driver-410
-
-# Reboot
 reboot
 
-# Validate installation
-cat /usr/local/cuda/version.txt
+
+# Validate installation ----
+cat /usr/local/cuda/version.json
+
+nvcc -V
 nvidia-smi
 
 
-# Install development and runtime libraries (~4GB)
-apt install -y --no-install-recommends \
-    cuda-10-0 \
-    libcudnn7=7.4.1.5-1+cuda10.0 \
-    libcudnn7-dev=7.4.1.5-1+cuda10.0
-apt update
-
-
-# Install TensorRT. Requires that libcudnn7 is installed above.
-apt install -y nvinfer-runtime-trt-repo-ubuntu1804-5.0.2-ga-cuda10.0
-apt update
-
-apt install -y --no-install-recommends libnvinfer-dev=5.0.2-1+cuda10.0
-
-
-
 # References ----
-# 1. https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1604&target_type=debnetwork
+# 1. https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_network
 # 2. https://www.tensorflow.org/install/gpu
+# 3. https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html
