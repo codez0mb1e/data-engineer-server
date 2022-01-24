645db939ce7cd1d69f01d4b42c7c1fa89ee856ac
codez0mb1e <d.petukhov@outlook.com>
2022-01-24 22:07:19 +0300

    Add .NET 6 support

--- scripts/install_dotnet_tools.sh
+++ scripts/install_dotnet_tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Install .NET 5.0 and Powershell Core
+# Install .NET and Powershell Core
 #
 
 
@@ -10,15 +10,16 @@
 # Download and register Microsoft repository GPG keys
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
-apt install -y wget apt-transport-https software-properties-common
+apt install -y apt-transport-https
 apt update
 
 
-# .NET 5.0 ----
+# Install .NET 5 and 6 ----
 
 # Install
-apt install -y dotnet-sdk-5.0
+apt install -y dotnet-sdk-5.0 dotnet-sdk-6.0
 
 # Validate
 dotnet new console
@@ -38,5 +39,5 @@ pwsh
 
 
 # References ----
-# 1. https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#1804-
+# 1. https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
 # 2. https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7.1
