94c35cf13182b1634bebd19ed5ea6a0238bbe6c2
codez0mb1e <d.petukhov@outlook.com>
2022-04-13 17:44:33 +0000

    Minor changes

--- scripts/kaggle_configure.sh
+++ scripts/kaggle_configure.sh
@@ -10,8 +10,11 @@ USR="dp"; readonly USR
 
 # install
 pip install --user $USR kaggle
-# now upload kaggle.json to ~/.kaggle/kaggle.json
+
+# now upload kaggle.json from https://www.kaggle.com/<username>/account to ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
 
 
 # References ----
 # 1. https://github.com/Kaggle/kaggle-api
+# 2. https://stackoverflow.com/questions/48370064/pip-install-kaggle-works-fine-but-kg-command-not-found
