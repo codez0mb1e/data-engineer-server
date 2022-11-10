3eb5ae7dc7d0156df0e6feebdd91359b4c41797c
codez0mb1e <d.petukhov@outlook.com>
2022-11-10 15:32:36 +0000

    Minor fixes

--- scripts/install_core.sh
+++ scripts/install_core.sh
@@ -12,6 +12,8 @@ whoami
 
 
 # Install updates ----
+sudo add-apt-repository universe
+
 sudo apt update
 apt list --upgradable
 sudo apt upgrade -y 
@@ -28,9 +30,10 @@ sudo apt install -y htop iftop iotop ncdu
 # Other  ----
 # disks stuff
 ls /mnt
+lsblk -f
 # mount data disc (optional)
-mkdir /datadrive
-mount /dev/sda1 /datadrive
+mkdir /mnt/datadrive
+mount /dev/sdb1 /mnt/datadrive
 df -Th
 
 # network stuff
