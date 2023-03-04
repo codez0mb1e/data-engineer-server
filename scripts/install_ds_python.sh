12933eb1ada4ab04cd9ab8575a424c87c55a01e1
codez0mb1e <d.petukhov@outlook.com>
2023-03-04 09:27:46 +0000

    Rethinking conda scripts structure

--- scripts/install_ds_python.sh
+++ scripts/install_ds_python.sh
@@ -5,7 +5,7 @@
 #
 
 
-# 0 .Set up python dev, pip, and virtualenv ----
+# 0. Set up python dev, pip, and virtualenv ----
 sudo apt install -y python3-dev python3-pip python3-virtualenv
 # or if already installed
 #> pip install --upgrade pip
@@ -20,44 +20,41 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 
 conda update conda
-# or for existing conda env:
-#> conda update --all --yes
-
-conda --version
-
-# get list of existing conda environments
-conda env list
+conda -V
 
 # config conda
 conda config --add channels conda-forge
 #conda config --add channels "Microsoft"
 conda config --set channel_priority strict
 
-# add new conda environment and install packages
+
+# 2. Set up DS environment ----
 NEW_CONDA_ENV="<your_data_science_env>"; readonly NEW_CONDA_ENV
 conda create -n $NEW_CONDA_ENV python=3.10
 
+conda env list
+
 # activate new environment
 conda activate $NEW_CONDA_ENV
-
 conda info
-conda list
 
+# install dev tools
+cat requirements.dev.txt
+pip install --upgrade -r requirements.dev.txt
+
+# install DS tools
+cat requirements.txt
+pip install --upgrade -r requirements.txt
+
+conda list
 
 # export new environment
 conda env export --no-builds > environment.yml
 nano environment.yml
 
 
-# 2. Set up DS environment ----
-
-# Install dev tools
-cat requirements.dev.txt
-pip install --upgrade -r requirements.dev.txt
-
-# Install DS tools
-cat requirements.txt
-pip install --upgrade -r requirements.txt
+# 3. Set up updates ----
+conda update --all --yes
 
 
 # References ----
