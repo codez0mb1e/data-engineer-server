41dc733dea2dadb741668f567eeb572778abc565
codez0mb1e <d.petukhov@outlook.com>
2023-04-30 08:24:03 +0000

    Commit signature verification

--- src/git_configure.sh
+++ src/git_configure.sh
@@ -39,7 +39,29 @@ mkdir ~/repos && cd ~/repos
 git clone git@github.com:codez0mb1e/cloud-rstudio-server.git
 
 
+# 3. Commit signature verification [4] ----
+# Check GPG version
+gpg --version # shoul be >= 2.1
+echo "test" | gpg --clearsign
+
+# Get list of existing keys
+gpg --list-secret-keys --keyid-format=long
+# or generate new one
+gpg --full-generate-key
+
+# Export public key
+# Open https://github.com/settings/gpg/new and paste public key from:
+gpg --armor --export <gpg_key_id>
+
+# Configure git to use GPG key
+git config --global user.signingkey <gpg_key_id>
+git config --global commit.gpgsign true
+
+[ -f ~/.bashrc ] && echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
+
+
 # References ----
 # 1. https://happygitwithr.com/push-pull-github.html
 # 2. https://github.com/settings/keys
 # 3. https://superuser.com/questions/215504/permissions-on-private-key-in-ssh-folder
+# 4. https://docs.github.com/en/authentication/managing-commit-signature-verification/about-commit-signature-verification
