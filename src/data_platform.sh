34bf59ad8ad9571678fe20717b057b16b52a5283
codez0mb1e <dmitry.petukhov@outlook.com>
2024-01-28 14:49:46 +0000

    Improvements and restructuring

--- src/data_platform.sh
+++ src/data_platform.sh
@@ -8,20 +8,24 @@
 # 1. Install ArangoDB [1, 2] ----
 ## Set params
 ARANGODB_INSTANCE="ariadne"; readonly ARANGODB_INSTANCE
-ARANGODB_VERSION="3.10.5.2"; readonly ARANGODB_VERSION
+ARANGODB_VERSION="3.11.6"; readonly ARANGODB_VERSION
 ARANGODB_PWD="***"; readonly ARANGODB_PWD
-ARAGONDB_DATA_DIR="/data/arangodb"; readonly ARAGONDB_DATA_DIR
+DATA_DIR="/data/arangodb"; readonly DATA_DIR
+LOG_DIR="/loga/arangodb"; readonly LOG_DIR
 
 ## Prepare
 docker pull arangodb/arangodb:${ARANGODB_VERSION}
-mkdir -p ${ARAGONDB_DATA_DIR}
+mkdir -p ${DATA_DIR}; mkdir -p ${LOG_DIR}
 
 ## Run
 docker run -e ARANGO_ROOT_PASSWORD=${ARANGODB_PWD} -d \
     -p 8529:8529 \
-    -v ${ARAGONDB_DATA_DIR}:/var/lib/arangodb3 \
+    -v ${DATA_DIR}:/var/lib/arangodb3 \
+    -v ${LOG_DIR}:/var/log/arangodb3 \
+    --restart unless-stopped
     --name ${ARANGODB_INSTANCE} \
     arangodb/arangodb:${ARANGODB_VERSION}
+    --log.output file:///var/log/arangodb3/dbserver.log
 
 ## Validate
 docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${ARANGODB_INSTANCE}
