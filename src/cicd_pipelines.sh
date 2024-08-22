64ceb941c6a274099c964c122ae68d23a447f193
codez0mb1e <dmitry.petukhov@outlook.com>
2024-08-22 16:25:29 +0000

    GitHub Actions (self-hosted agent)

---
+++ src/cicd_pipelines.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Configure CI/CD pipelines
+#
+
+
+# 1. GitHub Actions (self-hosted agent) ----
+# Set up
+# Go to the repository settings -> Actions -> Add runner
+# https://github.com/codez0mb1e/<your_repo>/settings/actions/runners/new?arch=x64&os=linux
+# and follow the instructions
+
+# Run as a service [1]
+sudo ./svc.sh install
+sudo ./svc.sh start
+
+# References
+# 1. https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/configuring-the-self-hosted-runner-application-as-a-service
