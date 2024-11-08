36dfdb7a0e56f298de8dce9557fa6ede659bf17c
codez0mb1e <d.petukhov@outlook.com>
2024-11-08 15:44:44 +0100

    Update .NET installation script

--- development/install_dotnet_tools.sh
+++ development/install_dotnet_tools.sh
@@ -4,21 +4,12 @@
 # Install .NET
 #
 
-
-# 0. Prepare ----
-# download and register Microsoft repository GPG keys (WARN: use only for Ubuntu 20.04)
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
 # 1. Install .NET 8 ----
 # install
-sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-8.0
+sudo apt install -y dotnet-sdk-8.0
 # validate
-dotnet --version
+dotnet --info
 
 
 # References ----
-# 1. https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
-# 2. https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-install?pivots=os-linux-ubuntu-2404&tabs=dotnet8
+# 1. https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-install?pivots=os-linux-ubuntu-2404&tabs=dotnet8
