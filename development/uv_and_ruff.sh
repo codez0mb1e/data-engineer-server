d2928da3fa427f519b01eda63ac22eadfe9108b7
codez0mb1e <dmitry.petukhov@outlook.com>
2025-08-27 16:36:10 +0200

    Refactor installation script for `uv` and `ruff`

--- development/uv_and_ruff.sh
+++ development/uv_and_ruff.sh
@@ -4,38 +4,54 @@
 # Install `uv` and `ruff` for Python development
 #
 
-# 0. Install uv ----
-curl -LsSf https://astral.sh/uv/install.sh | sh
-uv --version
 
+# 0. Install uv ----
+# install
+curl -LsSf https://astral.sh/uv/install.sh | sh
+# check
+uv -V
+# turn on auto-updates
 uv self update
 
-#
+
+# 1. Install Python tools for uv ----
+# Install Python 3.12
 uv python install 3.12
 uv python list 3.12
 echo 'import platform; print(platform.python_version())' | uv run -
 
 uv python upgrade 3.12
 
-#
+# Install venv
 uv venv
 
 
-# 1. Install ruff ----
-uv tool install ruff
+# Install ruff (globally)
+uv tool install ruff@latest
 ruff --version
 
 uv tool upgrade ruff
+
+
+# 2. Create a new project ----
+cd projects/
+uv init -n ai-agents
+cd ai-agents
+
+uv add ruff # if not installed globally yet
+uv lock
+
 
 # 2. Configure ruff for Python projects ----
 mkdir -p ~/.config/ruff
 cat <<EOF > ~/.config/ruff/config.toml
 [tool.ruff]
-line-length = 88
+line-length = 120
 select = ["E", "F", "W", "C90"]
 exclude = ["__pycache__", "venv", ".git", ".ruff_cache"]
 fix = true
 target-version = "py312"
+
 [tool.ruff.per-file-ignores]
 "__init__.py" = ["F401"]
 EOF
@@ -45,17 +61,7 @@ echo "Ruff configuration file created at ~/.config/ruff/config.toml"
 ruff check --config ~/.config/ruff/config.toml --fix --exit-zero
 echo "Ruff configuration validated successfully."
 
-# 3. Create uv virtual environment for Ruff ----
-cd apps/
-uv init ds-agents
-cd ds-agents
-
-uv add ruff
-uv lock
-
-
-
 
 # References ----
-# 1. https://docs.astral.sh/uv/
+# 1. https://docs.astral.sh/uv/getting-started/features/
 # 2. https://marketplace.visualstudio.com/items?itemName=charliermarsh.ruff
