edc60877e8de1634cf4a5880758c0d62f19fe9ee
codez0mb1e <dmitry.petukhov@outlook.com>
2025-08-27 16:36:45 +0200

    Reorganize AI agents setup script for improved clarity and structure

--- ai-agents/agents.sh
+++ ai-agents/agents.sh
@@ -4,12 +4,30 @@
 # AI agents development environment setup
 #
 
-uv add ikernel
-uv add "autogen-core" "autogen-agentchat" "autogen-ext[openai,ollama,azure]"
-uv add "autogen-ext[web-surfer]"
 
+# 0. Install Ollama [1]
+cd ~
+curl -fsSL https://ollama.com/install.sh | sh
+
+# checks
+ollama -v
+
+# load LLMs
+ollama run llama3.2
+ollama run deepseek-coder
+
+
+
+# 1. Install AutoGen ----
+uv sync --all-extras
+
+uv add "autogen-core" "autogen-agentchat"
+uv add "autogen-ext[web-surfer,openai,ollama,azure]"
+
+
+# 2. Install Playwright ----
 playwright install 
 
-# Download ollama https://ollama.com/download/mac
-# install on ubuntu: https://github.com/ollama/ollama/blob/main/docs/linux.md
-ollama run llama3.2
+
+# References:
+# 1. https://github.com/ollama/ollama/blob/main/docs/linux.md
