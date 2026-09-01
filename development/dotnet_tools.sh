423b07c2956feb73741b015544e6c842e317bc96
codez0m1e <d.petukhov@outlook.com>
2026-09-01 15:13:54 +0000

    Update to install .NET 10

--- development/dotnet_tools.sh
+++ development/dotnet_tools.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 #
-# Install .NET
+# Install .NET 10 Development Tools
 #
 
-# 1. Install .NET 9 ----
+# 1. Install .NET 10 ----
 # install
-sudo add-apt-repository ppa:dotnet/backports
+sudo apt install -y dotnet-sdk-10.0
+sudo apt install -y aspnetcore-runtime-10.0
 
-sudo apt update && \
-  sudo apt install -y dotnet-sdk-9.0
 # validate
 dotnet --info
 
+# trust HTTPS dev certs (required for local web/API debugging)
+dotnet dev-certs https --trust
+
+# 2. Install VS Code extensions for .NET development ----
+if command -v code &> /dev/null; then
+  code --install-extension ms-dotnettools.csdevkit          # C# Dev Kit
+  code --install-extension ms-dotnettools.csharp            # C# (OmniSharp/Roslyn)
+  code --install-extension ms-dotnettools.vscode-dotnet-runtime
+fi
+
 
 # References ----
-# 1. https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-install?pivots=os-linux-ubuntu-2404&tabs=dotnet9
+# 1. https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu-install?pivots=os-linux-ubuntu-2604&tabs=dotnet10
